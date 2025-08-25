b152afba49c547413bdafbb0c379a0a8620f069d
Martín Ferrer Canese <110267782+MartinFerrer@users.noreply.github.com>
2025-08-25 15:36:21 -0300

    Incluir aplicacion y urls en app principal de django
    TODO: configurar bien urls, renombrar app a nombre correcto

--- app/apps/accounts/urls.py
+++ app/apps/accounts/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path("", views.home, name="home"),  # TODO: Modulo de presentacion?
+    path("register/", views.register, name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="accounts/login.html", redirect_authenticated_user=True),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 ]
