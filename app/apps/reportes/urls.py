bf7d5bc0ef0304249e625107c491b7be5ee1300b
Martín Ferrer Canese <110267782+MartinFerrer@users.noreply.github.com>
2025-08-26 20:50:54 -0300

    Feature/Scrum 108: Configurar directorios y estructura de proyecto Django (#6)
    * Agregar carpeta IA_Chats con link_chats.txt
    
    * Agregar apps y urls de ejemplo para nuestros modulos

--- app/apps/reportes/urls.py
+++ app/apps/reportes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "presentacion"
+app_name = "apps.reportes"
 
 urlpatterns = [
     path("", views.ejemplo, name="ejemplo"),
--- app/apps/usuarios/urls.py
+++ app/apps/usuarios/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "presentacion"
+app_name = "usuarios"
 
 urlpatterns = [
     path("", views.ejemplo, name="ejemplo"),
