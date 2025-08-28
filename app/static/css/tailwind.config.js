9d9cd814bcac3c55d4b76e2ff9b1be2235b3c78e
Martín Ferrer Canese <110267782+MartinFerrer@users.noreply.github.com>
2025-08-28 15:41:04 -0300

    Cambiar github actions CI/CD

---
+++ app/static/css/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        "../../templates/**/*.html",
+        "../../**/templates/**/*.html",
+        "../../apps/panel_admin/templates/**/*.html",
+        "../../**/apps/panel_admin/templates/**/*.html",
+
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
