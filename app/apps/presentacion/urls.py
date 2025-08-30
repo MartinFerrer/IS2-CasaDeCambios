9c09cb6873ab871594893f40cff6290f67ff08b6
Iantorresm <iantorressm@gmail.com>
2025-08-30 15:39:35 -0300

    Menú principal y plantillas para las vistas propuestas desde los botones en el menú

--- app/apps/presentacion/urls.py
+++ app/apps/presentacion/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name = "presentacion"
+app_name = "transacciones"
 
 urlpatterns = [
-    path("", views.ejemplo, name="ejemplo"),
+    path("", views.home, name="home"),
 ]
