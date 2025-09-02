a760023b5a68b4e1661d4996a6c13fb4aa431310
Iantorresm <iantorressm@gmail.com>
2025-09-02 16:49:51 -0300

    Módulo de tasas funcional, se aññadió separador de tasas activas-inactivas y funcionalidad al botón de lista para activar/desactivar. Se hicieron cambios de nombre Moneda - >Divisa para "mantener la metáfora" . Queda mejorar aspecto visual y flujo del módulo divisas

--- app/apps/operaciones/urls.py
+++ app/apps/operaciones/urls.py
@@ -7,11 +7,11 @@ app_name = "operaciones"
 
 urlpatterns = [
     # URLs para el CRUD de Tasas de Cambio
-    path("tasas-de-cambio/", views.tasa_cambio_listar, name="tasa_cambio_listar"),
-    path("tasas-de-cambio/crear/", views.tasa_cambio_crear, name="tasa_cambio_crear"),
-    path("tasas-de-cambio/<uuid:pk>/editar/", views.tasa_cambio_editar, name="tasa_cambio_editar"),
-    path("tasas-de-cambio/<uuid:pk>/desactivar/", views.tasa_cambio_desactivar, name="tasa_cambio_desactivar"),
-    path("tasas-de-cambio/<uuid:pk>/activar/", views.tasa_cambio_activar, name="tasa_cambio_activar"),
+    path("admin/tasas-de-cambio/", views.tasa_cambio_listar, name="tasa_cambio_listar"),
+    path("admin/tasas-de-cambio/crear/", views.tasa_cambio_crear, name="tasa_cambio_crear"),
+    path("admin/tasas-de-cambio/<uuid:pk>/editar/", views.tasa_cambio_editar, name="tasa_cambio_editar"),
+    path("admin/tasas-de-cambio/<uuid:pk>/desactivar/", views.tasa_cambio_desactivar, name="tasa_cambio_desactivar"),
+    path("admin/tasas-de-cambio/<uuid:pk>/activar/", views.tasa_cambio_activar, name="tasa_cambio_activar"),
     # URL para la vista que muestra el listado de todas las divisa
     path("divisa/", views.divisa_listar, name="divisa_list"),
     # URL para la vista que crea una nueva divisa
