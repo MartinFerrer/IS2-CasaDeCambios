64ab41aaf31974c58d18d40993b2a60ff7d8c0f6
dai1dai <fabrizziokawabata@gmail.com>
2025-10-02 19:26:46 -0300

    TODOs ya resueltos del commit 8ef0926 Cambios realizados - Campos de textos ya adaptados al formato latinoamericano - Página de simulación y realización de transacciones ya adaptados al formato latinoamericano.

--- app/global_exchange_django/locale/es_PY/formats.py
+++ app/global_exchange_django/locale/es_PY/formats.py
@@ -1,7 +1,7 @@
 # locale/es_PY/formats.py
 
 NUMBER_GROUPING = 3  # Agrupar cada 3 dígitos
-THOUSAND_SEPARATOR = '.'  # Punto para separar miles
+THOUSAND_SEPARATOR = ' '  # Punto para separar miles
 DECIMAL_SEPARATOR = ','   # Coma para decimales
 
 # Configuración de formato de números para América Latina
